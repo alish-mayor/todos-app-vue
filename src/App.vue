277d91ab8c7160344805abb66db7b3775482737d
alish-mayor <alish22061999@gmail.com>
2021-11-30 23:44:49 +0600

    app base elements header and input designed

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="container">
+      <div class="header">
+        <h1 class="title">Tasks to do</h1>
+        <button class="plus-btn">+</button>
+      </div>
+      <div class="add-task">
+        <input type="text" class="add-task__input">
+        <button class="add-task__btn">Submit</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld,
+    
   },
 });
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+*, *::after, *::before{
+  box-sizing: border-box;
+}
+
+:root{
+  font-size: 10px;
+  
+}
+
+body{
+  background: #e7e7e7;
+  display: flex;
+  justify-content: center;
+  align-items: top;
+  min-height: 100vh;
+  
+}
+
+.container{
+  background: #ffffff;
+  min-width: 30rem;
+  padding: 1rem;
+  border-radius: 0.5rem;
+}
+
+.title{
+  display: inline-block;
+  font-weight: 400;
+}
+
+.add-task{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 </style>
