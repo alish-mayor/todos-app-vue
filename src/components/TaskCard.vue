d5c93ee373e430f15516d4aaad691dcaf5796a13
alish-mayor <alish22061999@gmail.com>
2021-12-04 21:23:48 +0600

    unnecessary code deleted and method renamed

--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -1,6 +1,6 @@
 <template>
         <v-card class="px-4 py-3 d-block mx-auto mt-3 text-center" width="280px">
-            <v-checkbox :label="taskText" class="ma-0" v-model="checked" @click="doing($event)"></v-checkbox>
+            <v-checkbox :label="taskText" class="ma-0" v-model="checked" @click="checkCompleted($event)"></v-checkbox>
             <v-btn class="font-weight-light text-capitalize">Change</v-btn>
             <v-btn class="ml-4 font-weight-light red white--text text-capitalize" @click="deleteTask(index)">Delete</v-btn>
         </v-card>
@@ -22,25 +22,11 @@ export default({
         deleteTask(index){
             this.$store.commit('deleteTask', index)
         },
-        increase(){
-            console.log('increase');
-        },
-        decrease(){
-            console.log('decrease');
-        },
-        doing(e){
-            if(this.checked){
-                this.$store.commit('increase');
-            } else {
-                this.$store.commit('decrease');
-            }
+        checkCompleted(e){
+            if(this.checked) this.$store.commit('increase'); 
+            else this.$store.commit('decrease');
         }
 
-    },
-    // computed: {
-    //     completed(){
-    //         return this.checked ? alert('yes') : alert('no');
-    //     }
-    // }
+    }
 })
 </script>
